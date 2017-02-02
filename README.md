e061b85993c24b015db17d686372659a4528102b
Brandon Chartier <brandon.chartier@gmail.com>
2017-02-02 15:47:57 -0500

    init repo

---
+++ README.md
@@ -0,0 +1,15 @@
+# Manifest Destiny
+
+Destined to expand across your project.
+
+## Install
+`npm install --save-dev manifest-destiny`
+
+## CLI Usage
+
+- `manifest [options]` - Run hash-brown using your options
+- `-f --files` - Set the files to be manifested
+- `-o --output` - Set the output path
+
+## Example
+`manifest --files="src/**/*.js" --output="out/manifest.json"`
---
+++ index.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+const argv = require('minimist')(process.argv.slice(2));
+const path = require('path');
+const fs = require('fs-promise');
+const glob = require('globby');
+
+const opt = {
+	files: argv.files || argv.f,
+	output: argv.output || argv.o
+};
+
+glob(opt.files).then(files => {
+	const map = files.map(file => {
+		const parsed = path.parse(file);
+
+		return {
+			name: parsed.name,
+			path: file
+		};
+	});
+
+	return fs.writeJson(opt.output, map.reduce((acc, x) => {
+		acc[x.name] = x.path;
+		return acc;
+	}));
+}).then(() => {
+	// OK
+}).catch(err => {
+	console.error(err);
+});
